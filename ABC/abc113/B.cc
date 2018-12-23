033c30d396c46cb1037cf1dfaf340345a6984fad
Akimoto Daichi <deltalphan+github@gmail.com>
2018-12-23 11:52:34 +0900

    update

--- ABC/abc113/B.cc
+++ ABC/abc113/B.cc
@@ -1,6 +1,23 @@
+#include <cmath>
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-  cout << "hello" << endl;
+  int N;
+  double T, A;
+  cin >> N >> T >> A;
+  double H;
+  int res;
+  double diff = 1000;
+  for (int i = 1; i <= N; ++i) {
+    cin >> H;
+    double diff_tmp = fabs(T - H * 0.006 - A);
+    if (diff_tmp < diff) {
+      diff = diff_tmp;
+      res = i;
+    }
+  }
+  cout << res << endl;
+
   return 0;
 }
