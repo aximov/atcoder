d73ea7205b95b746c20f82ea5b1483c7bf0cb511
Akimoto Daichi <deltalphan+github@gmail.com>
2019-05-26 23:34:58 +0900

    解説を読んで簡素化

--- ABC/ABC128/B.cc
+++ ABC/ABC128/B.cc
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <map>
 #include <algorithm>
-#include <set>
 #include <utility>
+#include <vector>
 
 using namespace std;
 
@@ -12,34 +10,17 @@ int main(){
   int N;
   cin >> N;
 
-  vector<int> restaurant_point; // id --> point
-  multimap<string, int> restaurant_location; // city name --> ids
-  set<string> locations;
+  vector<pair<pair<string, int>, int>> p(N);
 
   string S;
   int P;
   for (int i = 0; i < N; ++i) {
     cin >> S >> P;
-    restaurant_point.push_back(P);
-    restaurant_location.insert(make_pair(S, i));
-    locations.insert(S);
+    p[i] = make_pair(make_pair(S, -P), i);
   }
 
-  for (auto l: locations){
-    map<int, int, greater<>> point_id;
-    while(true) {
-      auto itr = restaurant_location.find(l);
-      if (itr == restaurant_location.end()) {
-        break;
-      } else {
-        point_id.insert(make_pair(restaurant_point[itr->second], itr->second));
-        restaurant_location.erase(itr);
-      }
-    }
-    for (auto e: point_id) {
-      cout << e.second + 1 << endl;
-    }
-  }
+  sort(p.begin(), p.end());
+  for (auto e: p) cout << e.second + 1 << endl;
 
   return 0;
 }
