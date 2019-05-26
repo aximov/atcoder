aac509cbf9f6b542daf441ac56b93ef884f03ace
Akimoto Daichi <deltalphan+github@gmail.com>
2019-05-26 23:23:53 +0900

    解説を読んで改善

--- ABC/ABC128/C.cc
+++ ABC/ABC128/C.cc
@@ -4,49 +4,34 @@
 using namespace std;
 
 int main(){
-  vector<int> primes {1,2,3,5,7,11,13,17,19,23,29};
-
   int N, M;
   cin >> N >> M;
 
-  vector<vector<int>> s;
+  vector<vector<int>> s(M);
   for (int i = 0; i < M; ++i) {
     int k;
     cin >> k;
 
-    vector<int> v;
+    s[i].resize(k);
     for (int j = 0; j < k; ++j) {
-      int s_tmp;
-      cin >> s_tmp;
-
-      v.push_back(s_tmp);
+      cin >> s[i][j];
+      --s[i][j];
     }
-    s.emplace_back(v);
   }
 
-  vector<int> p;
-  for (int i = 1; i <= M; ++i) {
-    int p_tmp;
-    cin >> p_tmp;
-    p.push_back(p_tmp);
+  vector<int> p(M);
+  for (int i = 0; i < M; ++i) {
+    cin >> p[i];
   }
 
   int ans = 0;
-  for (int i = 0; i <= N; ++i) {
-
-    long long pattern = 1;
-    for (int j = 0; j < 10; ++j) {
-      int b = 1<<j;
-      if (i & b) {
-        pattern *= primes[j];
-      }
-    }
-
+  for (int i = 0; i < (1 << N); ++i) {
     bool flg = true;
+
     for (int m = 0; m < M; ++m) {
       int cnt = 0;
       for (auto sw: s[m]) {
-        if (pattern % primes[sw] == 0) ++cnt;
+        if ((i >> sw) & 1) ++cnt;
       }
       if (cnt % 2 != p[m]) flg = false;
     }
